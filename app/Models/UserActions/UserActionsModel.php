18a6d2f97fbd79ed8d07b73fcb340c2236416160
ubuntu <ashutosh.gupta@velocis.co.in>
2024-08-20 14:37:17 +0530

    trash in caveat

--- app/Models/UserActions/UserActionsModel.php
+++ app/Models/UserActions/UserActionsModel.php
@@ -1,44 +1,34 @@
 <?php
+
 namespace App\Models\UserActions;
 
 use CodeIgniter\Model; 
-class UserActions_model extends Model {
+class UserActionsModel extends Model {
 
     function __construct() {
-// Call the Model constructor
+        // Call the Model constructor
         parent::__construct();
     }
 
     function finalSubmit($registration_id, $next_stage) {
-
         $curr_time = date('Y-m-d H:i:s');
-
         $status = FALSE;
-
         $this->db->trans_start();
-
         if ($next_stage == Initial_Approaval_Pending_Stage) {
             $status = $this->assignEfilingNumber($registration_id, session()->get('estab_details')['efiling_for_type_id'], session()->get('estab_details')['efiling_for_id'], $curr_time);
         }
-
         if ($next_stage == Initial_Defects_Cured_Stage) {
-
             $status = $this->updateDefects($registration_id, $curr_time);
         }
-
         if ($next_stage == I_B_Defected_Stage) {
             $status = TRUE;
         }
-
         if ($status) {
-
             $status = $this->update_EfilingNum_NextStage($registration_id, $next_stage, $curr_time);
-
             if ($status) {
                 $this->db->trans_complete();
             }
         }
-
         if ($this->db->trans_status() === FALSE) {
             return FALSE;
         } else {
@@ -47,15 +37,10 @@ class UserActions_model extends Model {
     }
 
     function trashSubmit($registration_id, $next_stage) {
-
         $curr_time = date('Y-m-d H:i:s');
-
         $status = FALSE;
-
         $status = $this->update_EfilingNum_NextStage($registration_id, $next_stage, $curr_time);
-
         if ($status) {
-
             return TRUE;
         } else {
             return FALSE;
@@ -63,7 +48,6 @@ class UserActions_model extends Model {
     }
 
     function updateDefects($registration_id, $curr_time) {
-
         $update_def = array(
             'is_active' => FALSE,
             'is_defect_cured' => TRUE,
@@ -75,7 +59,6 @@ class UserActions_model extends Model {
         $builder->WHERE('registration_id', $registration_id);
         $builder->WHERE('is_active', TRUE);
         $builder->UPDATE($update_def);
-
         if ($builder->affectedRows() > 0) {
             return TRUE;
         } else {
@@ -84,7 +67,6 @@ class UserActions_model extends Model {
     }
 
     function update_EfilingNum_NextStage($registration_id, $next_stage, $curr_time) {
-
         $update_data = array(
             'deactivated_on' => $curr_time,
             'is_active' => FALSE,
@@ -92,13 +74,10 @@ class UserActions_model extends Model {
             'updated_by_ip' => getClientIP()
         );
         $builder = $this->db->table('efil.tbl_efiling_num_status');
-
         $builder->WHERE('registration_id', $registration_id);
         $builder->WHERE('is_active', TRUE);
         $builder->UPDATE($update_data);
-
-        if ($$builder->affectedRows() > 0) {
-
+        if ($this->db->affectedRows() > 0) {
             $insert_data = array(
                 'registration_id' => $registration_id,
                 'stage_id' => $next_stage,
@@ -107,10 +86,9 @@ class UserActions_model extends Model {
                 'activated_by' => session()->get('login')['id'],
                 'activated_by_ip' => getClientIP()
             );
-
             $builder = $this->db->table('efil.tbl_efiling_num_status');
             $builder->INSERT($insert_data);
-            if ($builder->insertId()) {
+            if ($this->db->insertId()) {
                 return TRUE;
             } else {
                 return FALSE;
@@ -121,41 +99,34 @@ class UserActions_model extends Model {
     }
 
     function assign_efiling_number($registration_id, $efiling_for_type_id, $efiling_for_id, $curr_time) {
-
         $query = "SELECT users.id,  users.ref_m_usertype_id, users.admin_for_type_id, users.admin_for_id, count(allocated_on) efiling_num_count
-                FROM efil.tbl_users users
-                LEFT JOIN efil.tbl_efiling_nums en ON en.allocated_to = users.id AND to_char(en.allocated_on,'YYYY-MM-DD')::date ='" . date('Y-m-d') . "'
-                WHERE users.admin_for_type_id ='" . $efiling_for_type_id . "' AND users.admin_for_id ='" . $efiling_for_id . "' AND
-                users.is_deleted IS FALSE AND
-                users.ref_m_usertype_id IN(" . USER_ADMIN . "," . USER_ACTION_ADMIN . ")                  
-                GROUP BY users.id,users.ref_m_usertype_id,users.admin_for_type_id,users.admin_for_id ORDER BY efiling_num_count";
-
-
+            FROM efil.tbl_users users
+            LEFT JOIN efil.tbl_efiling_nums en ON en.allocated_to = users.id AND to_char(en.allocated_on,'YYYY-MM-DD')::date ='" . date('Y-m-d') . "'
+            WHERE users.admin_for_type_id ='" . $efiling_for_type_id . "' AND users.admin_for_id ='" . $efiling_for_id . "' AND
+            users.is_deleted IS FALSE AND
+            users.ref_m_usertype_id IN(" . USER_ADMIN . "," . USER_ACTION_ADMIN . ")                  
+            GROUP BY users.id,users.ref_m_usertype_id,users.admin_for_type_id,users.admin_for_id ORDER BY efiling_num_count";
         $query = $this->db->query($query);
         if ($query->getNumRows() >= 1) {
-
             $builder = $this->db->table('efil.tbl_efiling_nums');
-
             $assign_to = $query->getResultArray();
-
             $nums_data_update = array(
                 'allocated_to' => $assign_to[0]['id'],
-                'allocated_on' => $curr_time);
-
+                'allocated_on' => $curr_time
+            );
             $builder->WHERE('registration_id', $registration_id);
             $builder->WHERE('is_active', TRUE);
             $builder->UPDATE($nums_data_update);
-
             if ($builder->affectedRows() > 0) {
-
-                $data = array('registration_id' => $registration_id,
+                $data = array(
+                    'registration_id' => $registration_id,
                     'admin_id' => $assign_to[0]['id'],
                     'allocated_on' => $curr_time,
                     'updated_by' => $_SESSION['login']['id'],
                     'updated_on' => $curr_time,
                     'update_ip' => getClientIP(),
-                    'reason_to_allocate' => NULL);
-
+                    'reason_to_allocate' => NULL
+                );
                 $builder->INSERT($data);
                 if ($builder->insertId()) {
                     return TRUE;
@@ -171,7 +142,6 @@ class UserActions_model extends Model {
     }
 
     function get_share_email_details($registration_id) {
-
         $builder = $this->db->table('efil.tbl_doc_share_email');
         $builder->SELECT("*");
         $builder->WHERE('registration_id', $registration_id);
@@ -184,5 +154,3 @@ class UserActions_model extends Model {
     }
 
 }
-
-?>
--- app/Views/documentIndex/documentIndex_caveat_list_view.blade.php
+++ app/Views/documentIndex/documentIndex_caveat_list_view.blade.php
@@ -30,71 +30,75 @@ $uris = service('uri');
                 <tbody>
                     <?php
                     $sr = 1;
-                    foreach ($efiled_docs_list as $doc_list) {
-                        ?>
-                        <tr>
-                            <td><?= $sr++; ?></td>                    
-                            <td>
-                                <?php
-                                if($uris->getSegment(1)== 'efiling_search') {
-                                    $encrypted_string = url_encryption($doc_list['doc_id']);
-                                    ?>
-                                    <?php //echo_data($doc_list['docdesc']); ?>
-                                    <!--<a target="_blank" href="<?php /*echo base_url('documentIndex/WithoutLoginViewIndex/' . $encrypted_string ); */?>">
-                                        <?php /*echo_data($doc_list['doc_title']); */?>
-                                        <img src="<?/*= base_url('assets/images/pdf.png') */?>">
-                                    </a>-->
+                    if(isset($efiled_docs_list) && !empty($efiled_docs_list)) {
+                        foreach ($efiled_docs_list as $doc_list) {
+                            ?>
+                            <tr>
+                                <td><?= $sr++; ?></td>                    
+                                <td>
+                                    <?php
+                                    if($uris->getSegment(1)== 'efiling_search') {
+                                        $encrypted_string = url_encryption($doc_list['doc_id']);
+                                        ?>
+                                        <?php //echo_data($doc_list['docdesc']); ?>
+                                        <!--<a target="_blank" href="<?php /*echo base_url('documentIndex/WithoutLoginViewIndex/' . $encrypted_string ); */?>">
+                                            <?php /*echo_data($doc_list['doc_title']); */?>
+                                            <img src="<?/*= base_url('assets/images/pdf.png') */?>">
+                                        </a>-->
 
-                                <?php } else { ?>
+                                    <?php } else { ?>
 
-                                <?php
+                                    <?php
 
-                                if (in_array($_SESSION['login']['ref_m_usertype_id'], $allowed_users) && in_array($_SESSION['efiling_details']['stage_id'], $user_stages)) {
-                                    ?>
-                                    <?php //echo_data($doc_list['docdesc']); ?>
+                                    if (in_array($_SESSION['login']['ref_m_usertype_id'], $allowed_users) && in_array($_SESSION['efiling_details']['stage_id'], $user_stages)) {
+                                        ?>
+                                        <?php //echo_data($doc_list['docdesc']); ?>
 
-                                    <!--<a target="_blank" href="<?php /*echo base_url($doc_list['file_path']); */?>">
-                                        <?php /*echo_data($doc_list['doc_title']); */?>
-                                        <img src="<?/*= base_url('assets/images/pdf.png') */?>">
-                                    </a>-->
+                                        <!--<a target="_blank" href="<?php /*echo base_url($doc_list['file_path']); */?>">
+                                            <?php /*echo_data($doc_list['doc_title']); */?>
+                                            <img src="<?/*= base_url('assets/images/pdf.png') */?>">
+                                        </a>-->
 
-                                <?php } elseif (in_array($_SESSION['login']['ref_m_usertype_id'], $allowed_admins) && in_array($_SESSION['efiling_details']['stage_id'], $admin_stages)) { ?>
-                                    <?php echo_data($doc_list['docdesc']); ?>
-                                    <!--<a target="_blank" href="<?php /*echo base_url($doc_list['file_path']); */?>">
-                                        <?php /*echo_data($doc_list['doc_title']); */?>
-                                        <img src="<?/*= base_url('assets/images/pdf.png') */?>">
-                                    </a>-->
-                                <?php } else { ?>
-                                    <?php echo_data($doc_list['docdesc']); ?>
-                                    <!--<a target="_blank" href="<?php /*echo base_url($doc_list['file_path']); */?>">
-                                        <?php /*echo_data($doc_list['doc_title']); */?>
-                                        <img src="<?/*= base_url('assets/images/pdf.png') */?>">
-                                    </a>-->
-                                <?php } ?>
-                                <?php }//end of else condition efiling_search .. ?>
-                            </td>
-
-                            <?php if ((in_array($_SESSION['login']['ref_m_usertype_id'], $allowed_users) && in_array($_SESSION['efiling_details']['stage_id'], $user_stages)) || (in_array($_SESSION['login']['ref_m_usertype_id'], $allowed_admins) && in_array($_SESSION['efiling_details']['stage_id'], $admin_stages))) {
-                                ?>
-                                <td><?= echo_data($doc_list['icmis_docnum'] . ' / ' . $doc_list['icmis_docyear']); ?></td>
-                            <?php } ?>
-                            <?php if ($uris->getSegment(2) != 'view') { ?>
-                                <td width="10%"> 
-                                    <?php if (isset($doc_list['is_admin_checked'])) { ?> 
-                                        <a onclick = "delete_index('<?php echo_data(url_encryption(trim($doc_list['doc_id'] . '$$' . $doc_list['registration_id']))); ?>')" class="btn btn-danger btn-xs"><i class="fa fa-trash"></i></a>
-                                        <?php } ?>
+                                    <?php } elseif (in_array($_SESSION['login']['ref_m_usertype_id'], $allowed_admins) && in_array($_SESSION['efiling_details']['stage_id'], $admin_stages)) { ?>
+                                        <?php echo_data($doc_list['docdesc']); ?>
+                                        <!--<a target="_blank" href="<?php /*echo base_url($doc_list['file_path']); */?>">
+                                            <?php /*echo_data($doc_list['doc_title']); */?>
+                                            <img src="<?/*= base_url('assets/images/pdf.png') */?>">
+                                        </a>-->
+                                    <?php } else { ?>
+                                        <?php echo_data($doc_list['docdesc']); ?>
+                                        <!--<a target="_blank" href="<?php /*echo base_url($doc_list['file_path']); */?>">
+                                            <?php /*echo_data($doc_list['doc_title']); */?>
+                                            <img src="<?/*= base_url('assets/images/pdf.png') */?>">
+                                        </a>-->
+                                    <?php } ?>
+                                    <?php }//end of else condition efiling_search .. ?>
                                 </td>
-                            <?php } ?>
+
+                                <?php if ((in_array($_SESSION['login']['ref_m_usertype_id'], $allowed_users) && in_array($_SESSION['efiling_details']['stage_id'], $user_stages)) || (in_array($_SESSION['login']['ref_m_usertype_id'], $allowed_admins) && in_array($_SESSION['efiling_details']['stage_id'], $admin_stages))) {
+                                    ?>
+                                    <td><?= echo_data($doc_list['icmis_docnum'] . ' / ' . $doc_list['icmis_docyear']); ?></td>
+                                <?php } ?>
+                                <?php if ($uris->getSegment(2) != 'view') { ?>
+                                    <td width="10%"> 
+                                        <?php if (isset($doc_list['is_admin_checked'])) { ?> 
+                                            <a onclick = "delete_index('<?php echo_data(url_encryption(trim($doc_list['doc_id'] . '$$' . $doc_list['registration_id']))); ?>')" class="btn btn-danger btn-xs"><i class="fa fa-trash"></i></a>
+                                            <?php } ?>
+                                    </td>
+                                <?php } ?>
+                                <?php
+                                if ($_SESSION['login']['ref_m_usertype_id'] == USER_ADMIN) {
+                                    //$lbl_verified = $doc_list['page_no']['is_admin_checked'] == 't' ? 'Yes' : '';
+                                    ?>                        
+                                    <!--<td>
+                                        <?php /*echo $lbl_verified; */?>
+                                    </td>-->
+                                <?php } ?>
+                            </tr>
                             <?php
-                            if ($_SESSION['login']['ref_m_usertype_id'] == USER_ADMIN) {
-                                //$lbl_verified = $doc_list['page_no']['is_admin_checked'] == 't' ? 'Yes' : '';
-                                ?>                        
-                                <!--<td>
-                                    <?php /*echo $lbl_verified; */?>
-                                </td>-->
-                            <?php } ?>
-                        </tr>
-                    <?php } ?>
+                        }
+                    }
+                    ?>
                 </tbody>
             </table>
             <?php if (!empty($uploaded_docs )) {
