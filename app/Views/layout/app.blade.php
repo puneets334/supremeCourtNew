80c413038a6758c2b00886d6619f76877f0d5292
puneets334 <puneets334@gmail.com>
2024-09-27 16:07:13 +0530

    loader

--- app/Views/layout/app.blade.php
+++ app/Views/layout/app.blade.php
@@ -18,9 +18,20 @@
 	<link href="<?= base_url() . 'assets' ?>/css/select2.min.css" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="<?= base_url() . 'assets/newAdmin/' ?>css/jquery.dataTables.min.css">
 	@stack('style')
+	<style>
+		#loader-wrapper {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			z-index: 1000; /* Ensure it's on top of other content */
+		}
+	</style>
 </head>
 
 <body>
+<div id="loader-wrapper">
+        <div id="loader"></div>
+    </div>
 	<div class="wrapper">
 		<!--header section-->
 		@include('layout.header')
@@ -69,7 +80,23 @@
 		});
 	</script>
 
-	
+<script>
+        $(document).ready(function() {
+            $('#loader-wrapper').show();
+            var loaderTimeout = setTimeout(function() {
+                $('#loader-wrapper').fadeOut('slow', function() {
+                    $('#content').fadeIn('slow');
+                });
+            }, 3000);
+
+            $(window).on('load', function() {
+                clearTimeout(loaderTimeout);
+                $('#loader-wrapper').fadeOut('slow', function() {
+                    $('#content').fadeIn('slow');
+                });
+            });
+        });
+	</script>
 	@stack('script')
 </body>
 
