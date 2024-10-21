34e2054976b5b7f6373347778fdb136d71e97b9a
puneets334 <puneets334@gmail.com>
2024-10-21 17:31:17 +0530

    t

---
+++ app/Views/errors/html/error_500.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>500 Error</title>
+</head>
+<body>
+    <h1>Somthing Went Wrong</h1>
+    <!-- <p>The page you requested could not be found.</p> -->
+</body>
+</html> 
