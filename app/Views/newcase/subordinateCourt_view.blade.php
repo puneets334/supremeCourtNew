04eec31d947238cb5d08cc12b116fd77d6e6e8a4
puneets334 <puneets334@gmail.com>
2024-12-04 18:49:31 +0530

    validation

--- app/Views/newcase/subordinateCourt_view.blade.php
+++ app/Views/newcase/subordinateCourt_view.blade.php
@@ -300,7 +300,7 @@
                                     <input type="text"
                                         name="hc_case_type_name"
                                         id="hc_case_type_name"
-                                        class="form-control cus-form-ctrl"
+                                        class="form-control cus-form-ctrl hcctname"
                                         placeholder="Enter Case Type Name..">
                                 </div>
                             </div>
@@ -458,7 +458,7 @@
                                     <input type="text"
                                         name="dc_case_type_name"
                                         id="dc_case_type_name"
-                                        class="form-control cus-form-ctrl"
+                                        class="form-control cus-form-ctrl dcctname"
                                         placeholder="Enter Case Type Name..">
                                 </div>
                             </div>
@@ -950,8 +950,10 @@
             if (case_type_selectedText === 'NOT IN LIST') {
                 $('.hc_case_type_name').show();
                 alert('Are you sure that it is not in list');
+                $('.hcctname').attr('required', true);
             } else {
                 $('.hc_case_type_name').hide();
+                $('.hcctname').attr('required', false);
             }
         });
         $('#dc_case_type_id').on('change', function() {
@@ -960,8 +962,10 @@
             if (case_type_selectedText === 'NOT IN LIST') {
                 $('.dc_case_type_name').show();
                 alert('Are you sure that it is not in list');
+                $('.dcctname').attr('required', true);
             } else {
                 $('.dc_case_type_name').hide();
+                $('.dcctname').attr('required', true);
             }
         });
         $('#agency_case_type_id').on('change', function() {
