8bca5cd873e43ce2bbe42e2ceea1852d78d3ee26
puneets334 <puneets334@gmail.com>
2024-12-11 11:04:28 +0530

    session msg issue

--- app/Controllers/OldCaseRefiling/FinalSubmit.php
+++ app/Controllers/OldCaseRefiling/FinalSubmit.php
@@ -6,8 +6,8 @@ use App\Controllers\BaseController;
 use App\Models\Common\CommonModel;
 use App\Models\ShareDoc\ShareDocsModel;
 
-if (!defined('BASEPATH'))
-    exit('No direct script access allowed');
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
 class FinalSubmit extends BaseController
 {
     protected $Common_model;
@@ -18,32 +18,22 @@ class FinalSubmit extends BaseController
         parent::__construct();
         $this->Common_model = new CommonModel();
         $this->Share_docs_model = new ShareDocsModel();
-        // $this->load->model('common/Common_model');
-        // $this->load->model('shareDoc/Share_docs_model');
-        // //$this->load->library('encrypt');
+        // $this->load->library('encrypt');
         // $this->load->library('encryption');
-
-
-
     }
 
     public function index()
     {
-    
         $allowed_users_array = array(USER_ADVOCATE, USER_IN_PERSON, USER_CLERK, USER_DEPARTMENT);
-
         if (!in_array($_SESSION['login']['ref_m_usertype_id'], $allowed_users_array)) {
             redirect('dashboard');
             exit(0);
         }
-
         if (empty($_SESSION['efiling_details']) || empty($_SESSION['estab_details'])) {
             redirect('dashboard');
             exit(0);
         }
-
         $registration_id = $_SESSION['efiling_details']['registration_id'];
-
         if ($_SESSION['efiling_details']['ref_m_efiled_type_id'] == E_FILING_TYPE_DEFICIT_COURT_FEE && (bool) $_SESSION['estab_details']['enable_payment_gateway']) {
             $next_stage = Transfer_to_IB_Stage;
         } elseif ($_SESSION['login']['ref_m_usertype_id'] == USER_DEPARTMENT || $_SESSION['login']['ref_m_usertype_id'] == USER_CLERK) {
@@ -66,7 +56,6 @@ class FinalSubmit extends BaseController
         $final_submit = TRUE;
         if ($final_submit) {
             $result = $this->Common_model->updateCaseStatus($registration_id, $next_stage);
-            pr($result);
             if ($result) {
                 $next_stage = REFILED_OLD_EFILING_CASE;
                 $result1 = $this->Common_model->updateCaseStatus($registration_id, $next_stage);
@@ -76,13 +65,10 @@ class FinalSubmit extends BaseController
                     $this->autoTransferRefiledOldEfilingCaseDocumentsToICMIS($registration_id, $_SESSION['efiling_details']['diary_no'], $_SESSION['efiling_details']['diary_year'], $_SESSION['efiling_details']['efiling_no'], $_SESSION['login']['aor_code']);
                 }
                 $user_name = $_SESSION['login']['first_name'] . ' ' . $_SESSION['login']['last_name'];
-
                 //code added on 27 jan 21 to get diary num for mesg
                 $diary_no = $this->Common_model->get_Diary_no_forMsg($registration_id);
                 $diaryno = $diary_no[0]["diaryno"];
-
                 //Code added on 25 jan 21 to send proper message to users other than login user.
-
                 //XXXXXXXXXXXXXXX START 23-03-2021 XXXXXXXXXXXX
                 $index_data = '';
                 $sentSMS = '';
@@ -91,102 +77,74 @@ class FinalSubmit extends BaseController
                 } else {
                     $efiled_docs_list_mail = NULL;
                 }
-
                 if (!empty($efiled_docs_list_mail)) {
-
                     foreach ($efiled_docs_list_mail as $doc_list) {
-                        
-
-                        /*$index_data .= '
-                                        <a href="' . base_url('documentIndex/viewIndexItem/' . url_encryption($doc_list['doc_id'].'##'.'pdfmail')) . '" target="_blank">' . $doc_list['doc_title'] . '<br>( ' . $doc_list['docdesc'] . ')</a> ';*/
-
+                        /*$index_data .= '<a href="' . base_url('documentIndex/viewIndexItem/' . url_encryption($doc_list['doc_id'].'##'.'pdfmail')) . '" target="_blank">' . $doc_list['doc_title'] . '<br>( ' . $doc_list['docdesc'] . ')</a> ';*/
                         // $key = $this->config->item('encryption_key');
                         $msg = $doc_list['doc_id'];
-
-                        //$encrypted_string = $this->encrypt->encode($msg , $key);
+                        // $encrypted_string = $this->encrypt->encode($msg , $key);
                         // $encrypted_string = $this->encryption->encrypt($msg);
                         $encrypted_string = integerEncreption($msg);
-
-                        $index_data .= '
-                                        <a href="' . base_url('documentIndex/WithoutLoginViewIndex/' . $encrypted_string) . '" target="_blank">' . $doc_list['doc_title'] . '<br>( ' . $doc_list['docdesc'] . ')</a> ';
-
-
-
-                        /*$index_data .= '
-                                        <a href="' . base_url('documentIndex/WithoutLoginViewIndex/' . $doc_list['doc_id']) . '" target="_blank">' . $doc_list['doc_title'] . '<br>( ' . $doc_list['docdesc'] . ')</a> ';*/
+                        $index_data .= '<a href="' . base_url('documentIndex/WithoutLoginViewIndex/' . $encrypted_string) . '" target="_blank">' . $doc_list['doc_title'] . '<br>( ' . $doc_list['docdesc'] . ')</a> ';
+                        /*$index_data .= '<a href="' . base_url('documentIndex/WithoutLoginViewIndex/' . $doc_list['doc_id']) . '" target="_blank">' . $doc_list['doc_title'] . '<br>( ' . $doc_list['docdesc'] . ')</a> ';*/
                     }
                 } else {
                     /*$index_data .= '<tr><td colspan="4" class="text-center">No record found.</td></tr>';*/
                     $index_data .= 'No record found.';
                 }
-
                 //XXXXXXXXXXXXXXXX END XXXXXXXXXXXX
-
-
-                $sentSMS .= "Document have been filed in Diary No. $diaryno as Efiling no. " . efile_preview($_SESSION['efiling_details']['efiling_no']) . " 
-by Advocate $user_name  and is pending for initial approval with efiling admin. - Supreme Court of India";
-//                 log_message('CUSTOM', "Document have been filed in Diary No. $diaryno as Efiling no. " . efile_preview($_SESSION['efiling_details']['efiling_no']) . " 
-// by Advocate $user_name  and is pending for initial approval with efiling admin. - Supreme Court of India");
-
+                $sentSMS .= "Document have been filed in Diary No. $diaryno as Efiling no. " . efile_preview($_SESSION['efiling_details']['efiling_no']) . " by Advocate $user_name  and is pending for initial approval with efiling admin. - Supreme Court of India";
+                // log_message('CUSTOM', "Document have been filed in Diary No. $diaryno as Efiling no. " . efile_preview($_SESSION['efiling_details']['efiling_no']) . " by Advocate $user_name  and is pending for initial approval with efiling admin. - Supreme Court of India");
                 $sentSMS .= '<br>';
-
                 $sentSMS .=  'Folowing Details Are ' . ' ' .  $index_data;
-
                 //till here
-
                 $subject = "Submitted : Efiling no. " . efile_preview($_SESSION['efiling_details']['efiling_no']);
-
-
                 $share_emails = $this->Common_model->get_share_email_details($registration_id);
-
                 if (!empty($share_emails)) {
                     foreach ($share_emails as $emails) {
-
-                        //send_mobile_sms($emails['email'], $sentSMS,SCISMS_Document_Filed_In_Diary);
-
+                        // send_mobile_sms($emails['email'], $sentSMS,SCISMS_Document_Filed_In_Diary);
                         $send_email = send_mail_msg($emails['email'], $subject, $sentSMS, ucwords($emails['name']));
-                        //if($send_email=='success'){
-                        $doc_email_updated = array(
-                            'id' => $emails['id'],
-                            'sent_on_mail' => date('Y-m-d H:i:s'),
-                            'sent_on_mail_flag' => 'T'
-                        );
-                        $share_emails_updated = $this->Share_docs_model->add_doc_share_email_updated($emails['id'], $doc_email_updated);
-                        //}
+                        // if($send_email=='success') {
+                            $doc_email_updated = array(
+                                'id' => $emails['id'],
+                                'sent_on_mail' => date('Y-m-d H:i:s'),
+                                'sent_on_mail_flag' => 'T'
+                            );
+                            $share_emails_updated = $this->Share_docs_model->add_doc_share_email_updated($emails['id'], $doc_email_updated);
+                        // }
                     }
                 }
-                //Code to sent mail to login user
+                // Code to sent mail to login user
                 $sentSMS = "Efiling no. " . efile_preview($_SESSION['efiling_details']['efiling_no']) . " has been submitted and is pending for initial approval with efiling admin. - Supreme Court of India";
                 // log_message('CUSTOM', "Efiling no. " . efile_preview($_SESSION['efiling_details']['efiling_no']) . " has been submitted and is pending for initial approval with efiling admin. - Supreme Court of India");
                 send_mobile_sms($_SESSION['login']['mobile_number'], $sentSMS, SCISMS_Initial_Approval);
                 send_mail_msg($_SESSION['login']['emailid'], $subject, $sentSMS, $user_name);
-
-                getSessionData('msg', '<div class="alert alert-success text-center"> E-filing number ' . efile_preview($_SESSION['efiling_details']['efiling_no']) . ' submitted successfully for approval of E-filing Admin.!</div>');
+                $this->session->setFlashData('msg', '<div class="alert alert-success text-center"> E-filing number ' . efile_preview($_SESSION['efiling_details']['efiling_no']) . ' submitted successfully for approval of E-filing Admin.!</div>');
                 $_SESSION['efiling_details']['stage_id'] = Initial_Approaval_Pending_Stage;
-               return redirect('miscellaneous_docs/view');
-                //redirect('dashboard');
+                return redirect()->to(base_url('miscellaneous_docs/view'));
+                // redirect('dashboard');
                 // exit(0);
             } else {
-                getSessionData('msg', '<div class="alert alert-danger text-center">Submition failed. Please try again!</div>');
+                $this->session->setFlashData('msg', '<div class="alert alert-danger text-center">Submition failed. Please try again!</div>');
                 // log_message('CUSTOM', "Miscellaneous Docs- Submition failed. Please try again!");
-                return redirect('miscellaneous_docs/view');
-                //redirect('dashboard');
+                return redirect()->to(base_url('miscellaneous_docs/view'));
+                // redirect('dashboard');
                 // exit(0);
             }
         }
     }
+
     public function test()
     {
         $registration_id = $_SESSION['efiling_details']['registration_id'];
-
         $diary_id = $_SESSION['efiling_details']['diary_no'] . $_SESSION['efiling_details']['diary_year'];
         $efiling_no = $_SESSION['efiling_details']['efiling_no'];
         $result = $this->autoTransferRefiledOldEfilingCaseDocumentsToICMIS($registration_id, $_SESSION['efiling_details']['diary_no'], $_SESSION['efiling_details']['diary_year'], $_SESSION['efiling_details']['efiling_no'], $_SESSION['login']['aor_code']);
     }
+
     // Auto Transfer to scrutiny changes : start
     public function autoTransferRefiledOldEfilingCaseDocumentsToICMIS($registration_id = null, $diary_no = null, $diary_year = null, $efiling_no = null, $refiled_by_aor_code = null)
     {
-        // pr($registration_id);
         if (empty($registration_id)) {
             return NULL;
         } else {
@@ -199,9 +157,6 @@ by Advocate $user_name  and is pending for initial approval with efiling admin. 
                 CURLOPT_CUSTOMREQUEST => 'POST',
                 CURLOPT_POSTFIELDS => array('registration_id' => $registration_id, 'diary_no' => $diary_no, 'diary_year' => $diary_year, 'efiling_no' => $efiling_no, 'refiled_by_aor_code' => $refiled_by_aor_code)
             ));
-
-
-
             /* $curl = curl_init();
             curl_setopt_array($curl, array(
                 CURLOPT_URL => ADMIN_SERVER_URL.'newcase/AutoDiaryGeneration/updateOldEfilingRefiledCase',
@@ -214,21 +169,16 @@ by Advocate $user_name  and is pending for initial approval with efiling admin. 
             curl_close($curl);
             $response=(array)json_decode($response,true);
             var_dump($response);exit();*/
-
-
-
             $response = curl_exec($curl);
             curl_close($curl);
             $response = json_decode($response, true);
-            // pr($response);
             // if ($response['status'] == 'success') {
             //     getSessionData('msg', '<div style="font-size=24px;font-weight: bolder" class="alert alert-success text-center font-weight-bold"> E-filing number ' . $efiling_no . ' has been re-filed successfully.</div>');
             //     $_SESSION['efiling_details']['stage_id'] = Initial_Approaval_Pending_Stage;
             // } else {
             //     getSessionData('msg', '<div class="alert alert-danger text-center text-bg">' . $response['message'] . '.</div>');
             // }
-
-           return redirect('oldCaseRefiling/view');
+           return redirect()->to(base_url('oldCaseRefiling/view'));
         }
     }
     // Auto Transfer to scrutiny changes: end
