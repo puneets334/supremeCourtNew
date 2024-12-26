348e9dda52a3f63c0df0d7f029b3cc6a47ebd81c
saurabh <saurabh885812@gmailcom>
2024-12-26 14:48:31 +0530

    cdn to offline

--- app/Views/case_status/case_status_view.blade.php
+++ app/Views/case_status/case_status_view.blade.php
@@ -1,6 +1,7 @@
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 <script type="text/javascript" src="<?= base_url() ?>/assets/js/case_status/common.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<link href="<?= base_url() . 'assets/newAdmin/' ?>css/bootstrap.min.css" rel="stylesheet"> 
+<script src="<?=base_url()?>assets/js/bootstrap.min.js"></script>
+
 <!-- Case Status modal-start-->
 <style>
     /* tr.hide-table-padding td {
